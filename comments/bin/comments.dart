b608af7b094224aa143469d758eb07d6bd9fbeda
sureshkumarmecbe <sureshkumarmecbe@gmail.com>
2024-03-21 15:30:54 +0530

    Third Commit

---
+++ comments/bin/comments.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Single Line Comment
+  print('Welcome to Dart Coding');
+
+  /* Multiline Comment
+  print('Welcome to Dart Coding');
+  print('Welcome to Dart Coding');
+  print('Welcome to Dart Coding');
+  */
+
+  /// Document Comment
+  /// When document comment created, it will generated automatically while enter key pressed
+  ///
+}
